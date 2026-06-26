583ccaaa55d4424ff2d5680c8a9ab8ed2d65a289
MK8909 <maharghakunduxe@gmail.com>
2026-06-26 13:03:53 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
-from langchain.tools import tool 
+from langchain.tools import tool
 import requests
 from bs4 import BeautifulSoup
 from tavily import TavilyClient
-import os 
+import os
+import streamlit as st
 from dotenv import load_dotenv
-from rich import print
 load_dotenv()
 
 tavily_key = None
